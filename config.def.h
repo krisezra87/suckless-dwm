6de888f47a7e759fb1554745e555a42faa0d0cb1
Kris Ezra <kris.ezra@gmail.com>
2019-11-20 22:21:19 -0500

    Added custom VM support

--- config.def.h
+++ config.def.h
@@ -85,11 +85,11 @@ static const char *termcmd[]  = { "st", NULL };
 static const char *qutecmd[]  = { "qutebrowser", NULL };
 static const char *slockcmd[]  = { "slock", NULL };
 static const char *rangercmd[]  = { "st", "-e", "ranger", NULL };
-static const char *matlabcmd[]  = { "matlab", "-desktop", "-nosplash", NULL };
 static const char *tbirdcmd[]  = { "thunderbird", NULL };
 static const char *vboxcmd[]  = { "virtualbox", NULL };
 static const char *chromecmd[]  = { "chromium", NULL };
-
+static const char *windowsvm[]  = {"/usr/lib/virtualbox/VirtualBoxVM", "--startvm", "{8d087cf1-c7d5-42ed-9495-b7ba2f1c282a}", NULL };
+static const char *webervm[]  = {"/usr/lib/virtualbox/VirtualBoxVM", "--startvm", "{17ce840e-4971-4b83-84b4-74c9e48533e9}", NULL };
 #include <X11/XF86keysym.h>
 static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
 static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
@@ -107,6 +107,8 @@ static Key keys[] = {
     { MODKEY,                       XK_e,      spawn,          {.v = rangercmd } },
     { MODKEY,                       XK_t,      spawn,          {.v = tbirdcmd } },
     { MODKEY,                       XK_v,      spawn,          {.v = vboxcmd } },
+    { MODKEY,                       XK_w,      spawn,          {.v = windowsvm } },
+    { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = webervm } },
     { MODKEY,                       XK_c,      spawn,          {.v = chromecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },

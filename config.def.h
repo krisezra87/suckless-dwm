209bdc5ad5bfcb061e149a8a84f2073d3802382d
Kris Ezra <kezra@purdue.edu>
2020-08-05 23:06:58 -0400

    Custom configuration

--- config.def.h
+++ config.def.h
@@ -1,13 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const int topbar             = 0;        /* 0 means bottom bar */
+static const char *fonts[]          = { "monospace:size=15" };
+static const char dmenufont[]       = "monospace:size=15";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -15,10 +15,23 @@ static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
+
+static const char wb_bg[]           = "#1c1c1c";
+static const char wb_fg[]           = "#f6f3e8";
+static const char wb_cur[]          = "#656565";
+static const char wb_sel[]          = "#444444";
+static const char wb_grey[]         = "#99968b";
+static const char wb_blue[]         = "#8ac6f2";
+static const char wb_red[]          = "#e5786d";
+static const char wb_yellow[]       = "#cae682";
+static const char wb_ltgreen[]      = "#e7f6da";
+static const char wb_green[]        = "#95e454";
+static const char wb_dkgrey[]       = "#8f8f8f";
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { wb_fg, wb_bg,  wb_ltgreen  },
 };
 static const unsigned int alphas[][3]      = {
 	/*               fg      bg        border     */
@@ -29,6 +42,10 @@ static const unsigned int alphas[][3]      = {
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+/* pdfpc configuration */
+static const char pdfpc_presenter_title[] = "pdfpc - presenter";
+static const char pdfpc_presentation_title[] = "pdfpc - presentation";
+
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
@@ -37,8 +54,10 @@ static const Rule rules[] = {
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "St",      NULL,     NULL,           0,         0,          1,           0,        -1 },
+	{ "st",      NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+    { NULL,       NULL, pdfpc_presenter_title,    0,  0,          0,           0,         2 },
+    { NULL,       NULL, pdfpc_presentation_title, 0,  0,          0,           0,         1 },
 };
 
 /* layout(s) */
@@ -49,7 +68,7 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
+	// { "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 };
 
@@ -68,11 +87,31 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *qutecmd[]  = { "qutebrowser", NULL };
+static const char *slockcmd[]  = { "slock", NULL };
+static const char *rangercmd[]  = { "st", "-e", "vifm", NULL };
+static const char *vboxcmd[]  = { "virtualbox", NULL };
+static const char *bravecmd[]  = { "brave", NULL };
+static const char *windowsvm[]  = {"/usr/lib/virtualbox/VirtualBoxVM", "--startvm", "{8d087cf1-c7d5-42ed-9495-b7ba2f1c282a}", NULL };
+static const char *webervm[]  = {"/usr/lib/virtualbox/VirtualBoxVM", "--startvm", "{17ce840e-4971-4b83-84b4-74c9e48533e9}", NULL };
+#include <X11/XF86keysym.h>
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 
 static Key keys[] = {
+	{ 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+	{ 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
+	{ 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+    { MODKEY,                       XK_q,      spawn,          {.v = qutecmd } },
+    { MODKEY,                       XK_e,      spawn,          {.v = rangercmd } },
+    { MODKEY,                       XK_v,      spawn,          {.v = vboxcmd } },
+    { MODKEY,                       XK_w,      spawn,          {.v = windowsvm } },
+    { MODKEY|ShiftMask,             XK_w,      spawn,          {.v = webervm } },
+    { MODKEY,                       XK_c,      spawn,          {.v = bravecmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -82,10 +121,11 @@ static Key keys[] = {
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
-	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
-	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY|ShiftMask,             XK_c,      quit,           {0} },
+    { MODKEY|ControlMask,           XK_c,      spawn,          {.v = slockcmd } },
+	{ MODKEY,                       XK_n,      setlayout,      {.v = &layouts[0]} },
+	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[1]} },
+    // { MODKEY,                       XK_g,      setlayout,      {.v = &layouts[3]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
@@ -103,7 +143,7 @@ static Key keys[] = {
 	TAGKEYS(                        XK_7,                      6)
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 };
 
 /* button definitions */
@@ -122,4 +162,3 @@ static Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
